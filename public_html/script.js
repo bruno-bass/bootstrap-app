b6f1bc016685216a3ff5c6c3705c970752bb20be
Bruno <brunohdbass@gmail.com>
2014-06-10 17:13:56 -0300

    Separaçao de JQuery

--- public_html/script.js
+++ public_html/script.js
@@ -5,7 +5,12 @@
  */
 
 $(document).ready(function(){
-    $('button').click(function(){
-        $('img').fadeOut('slow');
-    });
-});
+                $('a').click(function(){
+                    $('img').css("width", "500px").css("height","500px");
+                    $('a').after('<h1>fechar</h1>');
+                    $('h1').click(function(){
+                        $('img').css('width', '200px').css('height', '200px');
+                        $('h1').hide();
+                    });
+                });
+            });
